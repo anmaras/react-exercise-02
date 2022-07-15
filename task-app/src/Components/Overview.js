4bdd9973df42141ffd92fab5eca5e9f0fd628287
Antonis <marasantonis@gmail.com>
2022-07-15 12:19:55 +0300

    add delete function

--- task-app/src/Components/Overview.js
+++ task-app/src/Components/Overview.js
@@ -6,35 +6,21 @@ class Overview extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      array: this.props.taskArray,
-    };
     this.getObjectId = this.getObjectId.bind(this);
   }
 
   getObjectId(data) {
-    this.setState({
-      array: this.state.array,
-    });
-    console.log(this.props.taskArray);
-    console.log(this.state.array);
-    // this.setState((prev) => ({
-    //   array: prev.array.filter((arr) => arr.id !== data),
-    // }));
-    // this.setState({
-    //   array: this.state.array.filter((arr) => arr.id !== data),
-    // });
+    this.props.objectKey(data);
   }
 
   render() {
     const { taskArray } = this.props;
-    console.log(this.state.array);
 
-    const taskList = taskArray.map((arr) => (
+    const taskList = taskArray.map((arr, index) => (
       <OverviewItem
         text={arr.title}
         key={arr.id}
-        number={arr.number}
+        number={index}
         id={arr.id}
         getId={this.getObjectId}
       />
