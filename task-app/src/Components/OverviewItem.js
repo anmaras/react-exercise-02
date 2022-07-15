9b3dd8f350c5fbeb88179f08f75cf7d4c1920201
Antonis <marasantonis@gmail.com>
2022-07-15 12:18:41 +0300

    add edit function on list item

--- task-app/src/Components/OverviewItem.js
+++ task-app/src/Components/OverviewItem.js
@@ -2,11 +2,53 @@ import React, { Component } from 'react';
 import './Overview.css';
 
 class OverviewItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: 'Edit',
+      isValid: true,
+      text: this.props.text,
+    };
+  }
+
+  changeEditTitle = () => {
+    this.state.isValid
+      ? this.setState({ title: 'Submit', isValid: !this.state.isValid })
+      : this.setState({ title: 'Edit', isValid: !this.state.isValid });
+  };
+
+  changeText = (e) => {
+    this.setState({
+      text: e.target.value,
+    });
+  };
+
   render() {
+    const isValid = this.state.isValid;
+
+    let editInput;
+
+    if (!isValid) {
+      editInput = (
+        <div>
+          <label></label>
+          <input
+            type="text"
+            onChange={this.changeText}
+            value={this.state.text}
+          ></input>
+        </div>
+      );
+    } else {
+      editInput = <div>{this.state.text}</div>;
+    }
+
     return (
       <li className="items">
         <button onClick={() => this.props.getId(this.props.id)}>Delete</button>
-        <div>{this.props.text}</div>
+        <button onClick={this.changeEditTitle}>{this.state.title}</button>
+        {editInput}
         <div>{this.props.number}</div>
       </li>
     );
